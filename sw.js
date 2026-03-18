fe4cc7dc9c14f7728197dbc4bdf775a8225bd909
mamatinbanbino-hash <mamatinbanbino@gmail.com>
2026-03-18 22:00:52 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'projecteur-pro-v2.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
